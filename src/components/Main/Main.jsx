15b23a806fe8309160fe2237e712cd7784ae5e4e
braunalex74 <braunalex74@googlemail.com>
2023-06-03 20:16:22 +0300

    add_CardImages

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,35 +4,93 @@ import {
   CardImage,
   CardText,
   MainContainer,
+  CardContainerBlog,
+  CardContainerBlogItem,
+  CardContainerImage,
+  CardTextBlogItem,
+  CardContainerBlogItemText,
+  CardTextBlogItemText,
+  CardImagesContainer,
 } from './Main.styled';
 
 import CardImage1 from '../assets/image/Card_1.png';
 import CardImage2 from '../assets/image/Card_2.png';
 import CardImage3 from '../assets/image/Card_3.png';
 import CardImage4 from '../assets/image/Card_4.png';
+import CardImage5 from '../assets/image/image_blog.jpg';
 
 const Main = () => {
+  const publications = [
+    {
+      image: CardImage1,
+      title: 'Індивідуальна психотерапія',
+    },
+    {
+      image: CardImage2,
+      title: 'Групові сесії',
+    },
+    {
+      image: CardImage3,
+      title: 'Психологічне консультування',
+    },
+    {
+      image: CardImage4,
+      title: 'Психотерапія online',
+    },
+    // Додайте сюди інші публікації
+  ];
+
   return (
     <MainContainer>
-      <CardContainer>
-        <CardImage src={CardImage1} alt="Card Image" />
-        <CardText>Індивідуальна психотерапія</CardText>
-      </CardContainer>
+      <CardContainerImage>
+        <CardContainer>
+          <CardImage src={CardImage1} alt="Card Image" />
+          <CardText>Індивідуальна психотерапія</CardText>
+        </CardContainer>
 
-      <CardContainer>
-        <CardImage src={CardImage2} alt="Card Image" />
-        <CardText>Групові сесії</CardText>
-      </CardContainer>
+        <CardContainer>
+          <CardImage src={CardImage2} alt="Card Image" />
+          <CardText>Групові сесії</CardText>
+        </CardContainer>
 
-      <CardContainer>
-        <CardImage src={CardImage3} alt="Card Image" />
-        <CardText>Психологічне консультування</CardText>
-      </CardContainer>
+        <CardContainer>
+          <CardImage src={CardImage3} alt="Card Image" />
+          <CardText>Психологічне консультування</CardText>
+        </CardContainer>
 
-      <CardContainer>
-        <CardImage src={CardImage4} alt="Card Image" />
-        <CardText>Психотерапія online</CardText>
-      </CardContainer>
+        <CardContainer>
+          <CardImage src={CardImage4} alt="Card Image" />
+          <CardText>Психотерапія online</CardText>
+        </CardContainer>
+      </CardContainerImage>
+      <CardContainerBlog>
+        <CardContainerBlogItem>
+          <CardImage src={CardImage5} alt="image_blog" />
+        </CardContainerBlogItem>
+        <CardContainerBlogItemText>
+          <CardTextBlogItem>
+            Відкинути минуле, хороше воно чи погане, і відкинути хороше чи
+            погане майбутнє, щоб просто бути. Бути - означає розвивати свою
+            унікальність, свою здатність бути живим, усім тим, хто Ти є, тут і
+            зараз.
+          </CardTextBlogItem>
+          <CardTextBlogItemText>
+            Психотерапія може допомогти, якщо Ти переймаєшся своїм емоційним
+            світом, маєш повторювані проблеми у відносинах або відчуваєш
+            труднощі з розумінням того, хто ТИ є. Аби пережити травму або
+            зіткнення з важкими життєвими ситуаціями не на одинці, Ти також
+            можеш звернутися до мене.
+          </CardTextBlogItemText>
+        </CardContainerBlogItemText>
+      </CardContainerBlog>
+      <CardImagesContainer>
+        {publications.map((publication, index) => (
+          <CardContainer key={index}>
+            <CardImage src={publication.image} alt="Card Image" />
+            <CardText>{publication.title}</CardText>
+          </CardContainer>
+        ))}
+      </CardImagesContainer>
     </MainContainer>
   );
 };
--- src/components/Main/Main.styled.jsx
+++ src/components/Main/Main.styled.jsx
@@ -1,12 +1,11 @@
 import styled from '@emotion/styled';
 
 export const MainContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 1200px;
+  display: block;
 `;
 
 export const CardContainer = styled.div`
+  display: block;
   width: 700px;
   height: 270px;
   margin-right: 100px;
@@ -41,3 +40,63 @@ export const CardText = styled.p`
   color: #093426;
   margin-top: 10px;
 `;
+
+export const CardContainerBlog = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CardContainerBlogItem = styled.div`
+  width: 400px;
+  height: 500px;
+  margin-right: 100px;
+  margin-top: 70px;
+  overflow: hidden;
+
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: box-shadow 0.3s ease;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const CardContainerImage = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1200px;
+`;
+
+export const CardTextBlogItem = styled.p`
+  font-family: 'Playfair Display';
+  font-style: italic;
+  font-weight: 600;
+  font-size: 24px;
+  width: 626px;
+
+  display: flex;
+  align-items: center;
+  text-align: right;
+
+  color: #093426;
+`;
+
+export const CardContainerBlogItemText = styled.ul``;
+
+export const CardTextBlogItemText = styled.p`
+  font-family: 'Playfair Display';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 30px;
+  width: 644px;
+
+  display: flex;
+  align-items: center;
+
+  color: #093426;
+`;
+
+export const CardImagesContainer = styled.div``;
